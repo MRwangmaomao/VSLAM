88a0a68cba86c5a3202ecfb0b4ab5e38b6ed072f
MRwangmaomao <30722103+MRwangmaomao@users.noreply.github.com>
2019-01-15 10:42:05 +0800

    Update parameters.cpp

--- VINS/VINS-Mono-code-annotation-master/feature_tracker/src/parameters.cpp
+++ VINS/VINS-Mono-code-annotation-master/feature_tracker/src/parameters.cpp
@@ -18,11 +18,19 @@ int FOCAL_LENGTH;
 int FISHEYE;
 bool PUB_THIS_FRAME;
 
+/**
+ * @brief 获取ros参数的模板函数
+ * 
+ * @tparam T 模板参数
+ * @param n ros节点
+ * @param name 参数名字
+ * @return T 返回模板对象
+ */
 template <typename T>
 T readParam(ros::NodeHandle &n, std::string name)
 {
     T ans;
-    if (n.getParam(name, ans))
+    if (n.getParam(name, ans))//getParam是ros中的方法
     {
         ROS_INFO_STREAM("Loaded " << name << ": " << ans);
     }
@@ -34,17 +42,22 @@ T readParam(ros::NodeHandle &n, std::string name)
     return ans;
 }
 
+/**
+ * @brief 前端的配置参数的获取
+ * 
+ * @param n ros节点
+ */
 void readParameters(ros::NodeHandle &n)
 {
     std::string config_file;
-    config_file = readParam<std::string>(n, "config_file");
-    cv::FileStorage fsSettings(config_file, cv::FileStorage::READ);
+    config_file = readParam<std::string>(n, "config_file");//"config_file"为ros launch的参数，在launch文件中配置作为args
+    cv::FileStorage fsSettings(config_file, cv::FileStorage::READ);// cv中以只读的方式打开文件 fsSettings文件读取对象
     if(!fsSettings.isOpened())
     {
         std::cerr << "ERROR: Wrong path to settings" << std::endl;
     }
-    std::string VINS_FOLDER_PATH = readParam<std::string>(n, "vins_folder");
-
+    std::string VINS_FOLDER_PATH = readParam<std::string>(n, "vins_folder");//"vins_folder"为ros launch的参数，在launch文件中配置作为args
+    
     fsSettings["image_topic"] >> IMAGE_TOPIC;
     fsSettings["imu_topic"] >> IMU_TOPIC;
     MAX_CNT = fsSettings["max_cnt"];
@@ -60,10 +73,10 @@ void readParameters(ros::NodeHandle &n)
         FISHEYE_MASK = VINS_FOLDER_PATH + "config/fisheye_mask.jpg";
     CAM_NAMES.push_back(config_file);
 
-    WINDOW_SIZE = 20;
+    WINDOW_SIZE = 20;//滑动窗大小
     STEREO_TRACK = false;
-    FOCAL_LENGTH = 460;
-    PUB_THIS_FRAME = false;
+    FOCAL_LENGTH = 460;//焦距
+    PUB_THIS_FRAME = false;//不发布当前帧
 
     if (FREQ == 0)
         FREQ = 100;
